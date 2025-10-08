84d877a94b4c74db8932436b968a354556f341cf
tametsi <93092155+tametsi@users.noreply.github.com>
2025-10-08 16:57:40 +0200

    chore(deps): add tailwindcss and daisyui

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [svelte()],
+	plugins: [svelte(), tailwindcss()],
 	server: { port: 4321 },
 	resolve: {
 		alias: {
