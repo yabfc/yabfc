0b6aee19b37939b20126f5b484d179898ba28bb9
melo-afk <git@melo.run>
2025-12-04 21:23:28 +0100

    feat: get default recipe (/chain) + improvements with the normal recipe chain

--- src/lib/logic/recipe.ts
+++ src/lib/logic/recipe.ts
@@ -1,7 +1,7 @@
 import type Recipe from '@/lib/models/recipe';
 import type Profile from '@/lib/models/profile';
 
-export function getRecipe(itemId: String, profile: Profile): Recipe[] {
+function getRecipe(itemId: String, profile: Profile): Recipe[] {
 	let recipes: Recipe[] = [];
 
 	for (let recipe of profile.recipes) {
@@ -11,52 +11,85 @@ export function getRecipe(itemId: String, profile: Profile): Recipe[] {
 			}
 		}
 	}
+	// add another condition here so feeding output of a refinery / blender is more feasable
+	for (let recipe of recipes) {
+		if (recipe.category === 'extractor' || recipe.category === 'extractor-miner') {
+			return [recipe];
+		}
+	}
 	return recipes;
 }
 
+function getDefaultRecipe(itemId: String, profile: Profile): Recipe | null {
+	for (let recipe of profile.recipes) {
+		if (recipe.default === undefined) {
+			continue;
+		}
+		for (let output of recipe.out) {
+			if (output.id === itemId && recipe.default) {
+				return recipe;
+			}
+		}
+	}
+	return null;
+}
+
+export function getDefaultRecipeChain(itemId: string, profile: Profile): Recipe[] | null {
+	let chain: Recipe[] = [];
+	let recipe = getDefaultRecipe(itemId, profile);
+	if (recipe == null) {
+		return null;
+	}
+
+	let childRecipes: Recipe[] = [];
+
+	for (const input of recipe.in) {
+		let childchains = getDefaultRecipeChain(input.id, profile);
+		if (childchains == null) {
+			return null;
+		}
+
+		if (childchains.length === 0) {
+			childRecipes = [];
+			break;
+		}
+		childRecipes = [...childRecipes, ...childchains];
+	}
+
+	return [recipe, ...childRecipes];
+}
+
 const memo = new Map<string, Recipe[][]>();
+
+type NestedRecipeChain = Array<NestedRecipeChain | Recipe>;
 
 export function getRecipeChain(
 	itemId: string,
 	profile: Profile,
 	path: string[] = [],
-	recursionLimit: number = 4,
-): Recipe[][] {
-	// avoid too deep recursion
-	if (path.includes(itemId) || path.length > recursionLimit) {
+): NestedRecipeChain {
+	// avoid possible recursion?
+	if (path.includes(itemId)) {
 		return [];
 	}
-	if (memo.has(itemId)) return memo.get(itemId)!;
 
-	let chain: Recipe[][] = [];
+	let recipes: NestedRecipeChain = [];
 	const nextPath = [...path, itemId];
-
 	for (const recipe of getRecipe(itemId, profile)) {
 		if (recipe.category === 'converter') {
 			continue;
 		}
-		let inputCombos: Recipe[][] = [[]];
-
+		let tmp: NestedRecipeChain = [];
 		for (const input of recipe.in) {
-			let childchains = getRecipeChain(input.id, profile, nextPath, recursionLimit);
-
-			if (childchains.length === 0) {
-				inputCombos = [[]];
+			let childRecipes = getRecipeChain(input.id, profile, nextPath);
+			if (childRecipes.length === 0) {
+				recipes.push([recipe]);
 				break;
 			}
-			let newCombos: Recipe[][] = [];
-			for (let combo of inputCombos) {
-				for (let childchain of childchains) {
-					newCombos.push([...combo, ...childchain]);
-				}
-			}
-			inputCombos = newCombos;
+			tmp.push([childRecipes]);
 		}
-
-		for (const combo of inputCombos) {
-			chain.push([recipe, ...combo]);
-		}
+		recipes.push([recipe, ...tmp]);
 	}
-	memo.set(itemId, chain);
-	return chain;
+
+	return recipes;
 }
