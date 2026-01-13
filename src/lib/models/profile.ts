ff5245d15e75a49b6593b808b9dfde621c9a0780
philxws692 <63463378+philxws692@users.noreply.github.com>
2026-01-13 09:12:11 +0100

    feat: add first recipe chain func

--- src/lib/models/profile.ts
+++ src/lib/models/profile.ts
@@ -4,6 +4,63 @@ import Recipe, { type RecipeInterface } from '@/lib/models/recipe';
 import EffectModule, { type EffectModuleInterface } from '@/lib/models/effect';
 import Research, { type ResearchInterface } from '@/lib/models/research';
 
+interface ProductionNode {
+	item: Item;
+	amount: number;
+	recipe?: Recipe;
+	children: ProductionNode[];
+}
+
+export class ProductionManager {
+	constructor(private profile: Profile) {}
+
+	public calculateRecipeChain(endproductId: string, requiredAmount: number = 1): ProductionNode | undefined {
+		console.log(endproductId)
+		const item = this.profile.items.find(i => i.id === endproductId);
+
+		if (item === undefined) {
+			console.warn("Invalid endproduct ID given.")
+			return;
+		}
+
+		if (item.category === "raw-resource" || item.id.includes("ore") || item.type === "fluid" ) {
+			return;
+		}
+
+		// Change to better priority matching (e.g. based on power, least machines, etc.)
+		const recipe = this.profile.recipes
+			.filter(r => r.out.some(output => output.id === endproductId))
+			.sort((a, b) => b.priority - a.priority)[0];
+
+		if (recipe === undefined) {
+			console.warn("No recipe found for item '" + item.getDisplayName + "'");
+			return;
+		}
+
+		const node: ProductionNode = {
+			item: item,
+			amount: requiredAmount,
+			recipe: recipe,
+			children: []
+		}
+
+		const outputDef = recipe.out.find(o => o.id === endproductId);
+		const amountPerCycle = outputDef?.amount || 1;
+
+		const neededCycles = requiredAmount / amountPerCycle;
+
+		for (const input of recipe.in) {
+			const totalInputNeeded = input.amount * neededCycles;
+
+			node.children.push(
+				this.calculateRecipeChain(input.id, totalInputNeeded)!
+			);
+		}
+
+		return node;
+	}
+}
+
 export interface ProfileInterface {
 	id: string;
 	name: string;
