65bcc9e9d56931abfe8eec4ad755a789cf76ca4c
melo-afk <git@melo.run>
2026-01-18 01:45:25 +0100

    feat: unfold items, machines, recipes for more information

--- src/lib/components/profile/ItemOverview.svelte
+++ src/lib/components/profile/ItemOverview.svelte
@@ -13,23 +13,52 @@
 				x.category.includes(searchQuery.toLowerCase()),
 		),
 	);
+	let expandedId = $state('');
+	const toggleId = (id: string) => {
+		expandedId = expandedId === id ? '' : id;
+	};
 </script>
 
 <SearchInput bind:value={searchQuery} />
 <ul class="list">
 	{#each filteredItems as item (item.id)}
 		<li class="list-row">
-			<div class="">
-				<AnvilIcon size="24" class="m-2" />
-			</div>
-			<div>
-				<div>{item.getDisplayName()}</div>
-				<div class="text-xs font-semibold uppercase opacity-60">
-					{item.category}
+			<div
+				class="collapse-arrow list-col-grow collapse list-item"
+				class:collapse-open={expandedId === item.id}
+			>
+				<button
+					class="collapse-title hover:bg-base-200 flex gap-4 p-0"
+					onclick={() => toggleId(item.id)}
+				>
+					<div class="">
+						<AnvilIcon size="24" class="m-2" />
+					</div>
+					<div class="text-left">
+						<div>{item.getDisplayName()}</div>
+						<div class="text-xs font-semibold uppercase opacity-60">
+							{item.category}
+						</div>
+					</div>
+				</button>
+				<div
+					class="collapse-content bg-base-200 !p-2"
+					class:hidden={expandedId !== item.id}
+				>
+					<div class="border-base-200">
+						<div>
+							<span class="text-xs uppercase opacity-50">Type:</span>
+							<span class="font-mono text-sm">{item.type ?? 'N/A'}</span>
+						</div>
+						<div>
+							<span class="text-xs uppercase opacity-50">Stack Size:</span>
+							<span class="font-mono text-sm">{item.stackSize ?? 'N/A'}</span>
+						</div>
+					</div>
 				</div>
 			</div>
 		</li>
 	{:else}
-		<p class="p-4">No items found for this profile.</p>
+		<p class="p-4 text-center">No items found for this profile.</p>
 	{/each}
 </ul>
--- src/lib/components/profile/RecipeOverview.svelte
+++ src/lib/components/profile/RecipeOverview.svelte
@@ -14,20 +14,55 @@
 					x.category.includes(searchQuery.toLowerCase())),
 		),
 	);
+	let expandedId = $state('');
+	const toggleId = (id: string) => {
+		expandedId = expandedId === id ? '' : id;
+	};
 </script>
 
 <SearchInput bind:value={searchQuery} />
 <ul class="list">
 	{#each filteredRecipes as recipe (recipe.id)}
 		<li class="list-row">
-			<div class="">
-				<DrillIcon size="24" class="m-2" />
-			</div>
-			<div>
-				<div>{recipe.getDisplayName()}</div>
-				<div class="text-xs font-semibold uppercase opacity-60">
-					<ArrowBigRightDashIcon size="16" class="inline" />
-					{recipe.out.map(x => profile.getItemById(x.id)?.name ?? x.id).join(', ')}
+			<div
+				class="collapse-arrow list-col-grow collapse list-item"
+				class:collapse-open={expandedId === recipe.id}
+			>
+				<button
+					class="collapse-title hover:bg-base-200 flex gap-4 p-0"
+					onclick={() => toggleId(recipe.id)}
+				>
+					<div class="">
+						<DrillIcon size="24" class="m-2" />
+					</div>
+					<div class="text-left">
+						<div>{recipe.getDisplayName()}</div>
+						<div class="text-xs font-semibold uppercase opacity-60">
+							<ArrowBigRightDashIcon size="16" class="inline" />
+							{recipe.out
+								.map(x => profile.getItemById(x.id)?.name ?? x.id)
+								.join(', ')}
+						</div>
+					</div>
+				</button>
+				<div
+					class="collapse-content bg-base-200 !p-2"
+					class:hidden={expandedId !== recipe.id}
+				>
+					<div class="border-base-200">
+						<div>
+							<span class="text-xs uppercase opacity-50">Category:</span>
+							<span class="font-mono text-sm">{recipe.category ?? 'N/A'}</span>
+						</div>
+						<div>
+							<span class="text-xs uppercase opacity-50">Duration:</span>
+							<span class="font-mono text-sm">{recipe.duration ?? 'N/A'}</span>
+						</div>
+						<div>
+							<span class="text-xs uppercase opacity-50">Limitations:</span>
+							<span class="font-mono text-sm">{recipe.limitations ?? 'N/A'}</span>
+						</div>
+					</div>
 				</div>
 			</div>
 		</li>
