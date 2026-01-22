3d1e6341a05ef06dba7961a16a7815137cde4eff
melo-afk <git@melo.run>
2026-01-22 18:21:51 +0100

    feat: add more costs

--- src/lib/models/profile.ts
+++ src/lib/models/profile.ts
@@ -3,87 +3,7 @@ import Item, { type ItemInterface } from '@/lib/models/item';
 import Recipe, { type RecipeInterface } from '@/lib/models/recipe';
 import EffectModule, { type EffectModuleInterface } from '@/lib/models/effect';
 import Research, { type ResearchInterface } from '@/lib/models/research';
-import solver from 'javascript-lp-solver';
-
-export class ProductionGraph {
-	private recipes: Recipe[];
-	private items: Item[];
-	private matrix: number[][]; // Rows: Items, Cols: Recipes
-	private itemIndexMap: Map<string, number>;
-	private recipeIndexMap: Map<string, number>;
-
-	constructor(profile: Profile) {
-		this.recipes = profile.recipes.filter(r => r.available);
-		this.items = profile.items;
-
-		this.itemIndexMap = new Map(this.items.map((it, i) => [it.id, i]));
-		this.recipeIndexMap = new Map(this.recipes.map((re, i) => [re.id, i]));
-
-		this.matrix = this.buildMatrix();
-	}
-
-	private buildMatrix(): number[][] {
-		const rows = this.items.length;
-		const cols = this.recipes.length;
-		const matrix = Array.from({ length: rows }, () => new Array(cols).fill(0));
-
-		this.recipes.forEach((recipe, colIdx) => {
-			const duration = recipe.duration;
-			recipe.in.forEach(io => {
-				const rowIdx = this.itemIndexMap.get(io.id);
-				if (rowIdx !== undefined) matrix[rowIdx][colIdx] -= io.amount / duration;
-			});
-			recipe.out.forEach(io => {
-				const rowIdx = this.itemIndexMap.get(io.id);
-				if (rowIdx !== undefined) matrix[rowIdx][colIdx] += io.amount / duration;
-			});
-		});
-		return matrix;
-	}
-
-	calculateOptimal(
-		itemId: string,
-		targetAmount: number,
-		weights: { power: number; resources: number; preference: number },
-	) {
-		const model: any = {
-			optimize: 'cost',
-			opType: 'min',
-			constraints: {},
-			variables: {},
-		};
-
-		// 1. Set constraints based on our Matrix (Each row is an item)
-		this.items.forEach(item => {
-			// We want the net production of each item to be >= 0 (no missing resources)
-			// Except for our target item, which must be >= targetAmount
-			const minRequired = item.id === itemId ? targetAmount : 0;
-			model.constraints[item.id] = { min: minRequired };
-		});
-
-		// 2. Set variables (Recipes)
-		this.recipes.forEach((recipe, colIdx) => {
-			const recipeVar: any = {};
-
-			// Map the matrix values to the solver constraints
-			this.items.forEach((item, rowIdx) => {
-				const flow = this.matrix[rowIdx][colIdx];
-				if (flow !== 0) recipeVar[item.id] = flow;
-			});
-
-			// 3. Add Pareto Costs (Weighting)
-			// You can change 'cost' based on user sliders
-			//const powerCost = this.getPowerUsage(recipe); // e.g., 30 for 30MW
-			// powerCost * weights.power +
-			const preferenceCost = recipe.priority * weights.preference;
-			recipeVar.cost = preferenceCost;
-
-			model.variables[recipe.id] = recipeVar;
-		});
-
-		return solver.Solve(model);
-	}
-}
+import solver, { type Model, type SolveResult } from 'javascript-lp-solver';
 
 export interface ProfileInterface {
 	id: string;
@@ -104,6 +24,7 @@ export default class Profile {
 	machines: Machine[];
 	machineEffects: EffectModule[];
 	research: Research[];
+	productionMatrix: number[][];
 	upload?: boolean;
 
 	constructor(profile: ProfileInterface, upload?: boolean) {
@@ -115,7 +36,77 @@ export default class Profile {
 		this.machines = profile.machines.map(x => new Machine(x));
 		this.machineEffects = profile.machineEffects.map(x => new EffectModule(x));
 		this.research = profile.research.map(x => new Research(x));
+		this.productionMatrix = this.buildProductionMatrix();
 		this.upload = upload;
+	}
+
+	private buildProductionMatrix(): number[][] {
+		const rows = this.items.length;
+		const recipes = this.recipes.filter(x => x.craftable !== false && x.available === true);
+		const matrix = Array.from({ length: rows }, () => new Array(recipes.length).fill(0));
+		let itemIndexMap = new Map(this.items.map((it, i) => [it.id, i]));
+
+		recipes.forEach((recipe, colIdx) => {
+			const duration = recipe.duration;
+			recipe.in.forEach(io => {
+				const rowIdx = itemIndexMap.get(io.id);
+				if (rowIdx !== undefined) matrix[rowIdx][colIdx] -= io.amount / duration;
+			});
+			recipe.out.forEach(io => {
+				const rowIdx = itemIndexMap.get(io.id);
+				if (rowIdx !== undefined) matrix[rowIdx][colIdx] += io.amount / duration;
+			});
+		});
+		return matrix;
+	}
+
+	calculateOptimal(
+		itemId: string,
+		targetAmount: number,
+		weights: { power: number; building: number; priority: number },
+	): SolveResult | undefined {
+		if (this.getItemById(itemId) === undefined) {
+			console.log('item does not exist');
+			return;
+		}
+
+		const model: Model = {
+			optimize: 'cost',
+			opType: 'min',
+			constraints: {},
+			variables: {},
+			options: {
+				timeout: 5000,
+			},
+		};
+
+		// net production of each item should be >= 0, (for the target item >= targetAmount)
+		this.items.filter(x => x.id != itemId).forEach(x => (model.constraints[x.id] = { min: 0 }));
+		model.constraints[itemId] = { equal: targetAmount };
+
+		// set variables so i.e the recipes
+		this.recipes
+			.filter(x => x.craftable !== false && x.available === true)
+			.forEach((recipe, colIdx) => {
+				const recipeVar: any = {};
+
+				// map the matrix values to the solver constraints
+				this.items.forEach((item, rowIdx) => {
+					const flow = this.productionMatrix![rowIdx][colIdx];
+					if (flow !== 0) recipeVar[item.id] = flow;
+				});
+
+				// cut the power down to MW, otherwise the cost get's way too high
+				const powerCost =
+					(weights.power * (this.getMinPowerConsumptionByRecipeId(recipe.id) ?? 0)) /
+					1_000_000;
+				const priorityCost = weights.priority * recipe.priority;
+				const buildingCost = weights.building * 1;
+				recipeVar.cost = powerCost + priorityCost + buildingCost;
+
+				model.variables[recipe.id] = recipeVar;
+			});
+		return solver.Solve(model) as SolveResult | undefined;
 	}
 
 	getItemById(id: string): Item | undefined {
@@ -130,11 +121,22 @@ export default class Profile {
 		return this.recipes.filter(x => x.out.find(io => io.id == id));
 	}
 
-	getMachinesByRecipe(recipe: Recipe): Machine[] {
-		return this.machines.filter(x => x.recipeCategories.includes(recipe.category));
+	getMinPowerConsumptionByRecipeId(id: string): number | undefined {
+		const recipe = this.getRecipeById(id);
+		if (recipe === undefined) return;
+		const valid_machines = this.machines.filter(x =>
+			x.recipeCategories.includes(recipe.category),
+		);
+		if (valid_machines.length === 0) {
+			return;
+		}
+		const minConsumption = valid_machines.reduce((min, machine) =>
+			machine.requiredPower < min.requiredPower ? machine : min,
+		);
+		return minConsumption.requiredPower;
 	}
 
-	getProductionGraph(): ProductionGraph {
-		return new ProductionGraph(this);
+	getMachinesByRecipe(recipe: Recipe): Machine[] {
+		return this.machines.filter(x => x.recipeCategories.includes(recipe.category));
 	}
 }
