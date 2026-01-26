18298cb728681f66a091712d87b0603a6a988602
tametsi <93092155+tametsi@users.noreply.github.com>
2026-01-26 12:55:32 +0100

    refactor(ui): update overview collapses

--- src/lib/components/profile/MachineOverview.svelte
+++ src/lib/components/profile/MachineOverview.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
+	import SearchInput from '@/lib/components/shared/Search.svelte';
 	import Profile from '@/lib/models/profile';
 	import { HammerIcon } from '@lucide/svelte';
-	import SearchInput from '@/lib/components/shared/Search.svelte';
 
 	let { profile }: { profile: Profile } = $props();
 	let searchQuery = $state('');
@@ -13,54 +13,49 @@
 				x.recipeCategories.some(x => x.includes(searchQuery.toLowerCase())),
 		),
 	);
-	let expandedId = $state('');
-	const toggleId = (id: string) => {
-		expandedId = expandedId === id ? '' : id;
-	};
 </script>
 
 <SearchInput bind:value={searchQuery} />
+
 <ul class="list">
 	{#each filteredMachines as machine (machine.id)}
-		<li class="list-row">
-			<div
-				class="collapse-arrow list-col-grow collapse list-item"
-				class:collapse-open={expandedId === machine.id}
+		<li class="list-row p-0">
+			<details
+				class="collapse-arrow list-col-grow collapse"
+				name="accordion-machine-overview"
 			>
-				<button
-					class="collapse-title hover:bg-base-200 flex gap-4 p-0"
-					onclick={() => toggleId(machine.id)}
-				>
-					<div class="">
+				<summary class="collapse-title select-none">
+					<div class="flex">
 						<HammerIcon size="24" class="m-2" />
-					</div>
-					<div class="text-left">
-						<div>{machine.getDisplayName()}</div>
-						<div class="text-xs font-semibold uppercase opacity-60">
-							{machine.recipeCategories}
+
+						<div>
+							<div>{machine.getDisplayName()}</div>
+							<div class="text-xs font-semibold uppercase opacity-60">
+								{machine.recipeCategories}
+							</div>
 						</div>
 					</div>
-				</button>
-				<div
-					class="collapse-content bg-base-200 !p-2"
-					class:hidden={expandedId !== machine.id}
-				>
-					<div class="border-base-200">
-						<div>
-							<span class="text-xs uppercase opacity-50">Required Power:</span>
+				</summary>
+
+				<div class="collapse-content">
+					<ul class="px-2">
+						<li>
+							<span class="text-base-content/50 text-xs uppercase">
+								Required Power:
+							</span>
 							<span class="font-mono text-sm">{machine.requiredPower ?? 'N/A'}</span>
-						</div>
-						<div>
-							<span class="text-xs uppercase opacity-50">Features:</span>
-							<span class="font-mono text-sm"
-								>{machine.features.map(x => x.id) ?? 'N/A'}</span
-							>
-						</div>
-					</div>
+						</li>
+						<li>
+							<span class="text-base-content/50 text-xs uppercase">Features:</span>
+							<span class="font-mono text-sm">
+								{machine.features.map(x => x.id).join(', ') ?? 'N/A'}
+							</span>
+						</li>
+					</ul>
 				</div>
-			</div>
+			</details>
 		</li>
 	{:else}
-		<p class="p-4">No machines found for this profile.</p>
+		<p class="p-4">No machines found.</p>
 	{/each}
 </ul>
--- src/lib/components/profile/RecipeOverview.svelte
+++ src/lib/components/profile/RecipeOverview.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
+	import SearchInput from '@/lib/components/shared/Search.svelte';
 	import Profile from '@/lib/models/profile';
 	import { ArrowBigRightDashIcon, DrillIcon } from '@lucide/svelte';
-	import SearchInput from '@/lib/components/shared/Search.svelte';
 
 	let { profile }: { profile: Profile } = $props();
 	let searchQuery = $state('');
@@ -14,59 +14,51 @@
 					x.category.includes(searchQuery.toLowerCase())),
 		),
 	);
-	let expandedId = $state('');
-	const toggleId = (id: string) => {
-		expandedId = expandedId === id ? '' : id;
-	};
 </script>
 
 <SearchInput bind:value={searchQuery} />
+
 <ul class="list">
 	{#each filteredRecipes as recipe (recipe.id)}
-		<li class="list-row">
-			<div
-				class="collapse-arrow list-col-grow collapse list-item"
-				class:collapse-open={expandedId === recipe.id}
-			>
-				<button
-					class="collapse-title hover:bg-base-200 flex gap-4 p-0"
-					onclick={() => toggleId(recipe.id)}
-				>
-					<div class="">
+		<li class="list-row p-0">
+			<details class="collapse-arrow list-col-grow collapse" name="accordion-recipe-overview">
+				<summary class="collapse-title select-none">
+					<div class="flex">
 						<DrillIcon size="24" class="m-2" />
-					</div>
-					<div class="text-left">
-						<div>{recipe.getDisplayName()}</div>
-						<div class="text-xs font-semibold uppercase opacity-60">
-							<ArrowBigRightDashIcon size="16" class="inline" />
-							{recipe.out
-								.map(x => profile.getItemById(x.id)?.name ?? x.id)
-								.join(', ')}
+
+						<div>
+							<div>{recipe.getDisplayName()}</div>
+							<div class="text-xs font-semibold uppercase opacity-60">
+								<ArrowBigRightDashIcon size="16" class="inline" />
+								{recipe.out
+									.map(x => profile.getItemById(x.id)?.name ?? x.id)
+									.join(', ')}
+							</div>
 						</div>
 					</div>
-				</button>
-				<div
-					class="collapse-content bg-base-200 !p-2"
-					class:hidden={expandedId !== recipe.id}
-				>
-					<div class="border-base-200">
-						<div>
-							<span class="text-xs uppercase opacity-50">Category:</span>
+				</summary>
+
+				<div class="collapse-content">
+					<ul class="px-2">
+						<li>
+							<span class="text-base-content/50 text-xs uppercase">Category:</span>
 							<span class="font-mono text-sm">{recipe.category ?? 'N/A'}</span>
-						</div>
-						<div>
-							<span class="text-xs uppercase opacity-50">Duration:</span>
+						</li>
+						<li>
+							<span class="text-base-content/50 text-xs uppercase">Duration:</span>
 							<span class="font-mono text-sm">{recipe.duration ?? 'N/A'}</span>
-						</div>
-						<div>
-							<span class="text-xs uppercase opacity-50">Limitations:</span>
-							<span class="font-mono text-sm">{recipe.limitations ?? 'N/A'}</span>
-						</div>
-					</div>
+						</li>
+						<li>
+							<span class="text-base-content/50 text-xs uppercase">Limitations:</span>
+							<span class="font-mono text-sm">
+								{recipe.limitations?.join(', ') ?? 'N/A'}
+							</span>
+						</li>
+					</ul>
 				</div>
-			</div>
+			</details>
 		</li>
 	{:else}
-		<p class="p-4">No recipes found for this profile.</p>
+		<p class="p-4">No recipes found.</p>
 	{/each}
 </ul>
