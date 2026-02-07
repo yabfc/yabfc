76485f0114a07e78d3905b8beed6819a15dca93c
tametsi <93092155+tametsi@users.noreply.github.com>
2026-02-07 20:15:46 +0100

    feat(ui): add factory details skeleton

---
+++ src/lib/components/profile/FactoryDetails.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import ItemSelect from '@/lib/components/shared/ItemSelect.svelte';
+	import Item from '@/lib/models/item';
+
+	let inputs = $state<{ item: Item; amount: number }[]>([]),
+		outputs = $state<{ item: Item; amount: number }[]>([]);
+
+	function calculate() {
+		//
+	}
+</script>
+
+<div
+	class="fixed top-24 bottom-4 left-4 z-10 h-fit max-h-[calc(100%-var(--spacing)*28)] w-80 overflow-auto"
+>
+	<div class="rounded-box bg-base-200 p-2">
+		<h2 class="text-xl font-bold">This Factory</h2>
+
+		<fieldset class="fieldset bg-base-100 border-base-300 rounded-box min-w-0 border p-4">
+			<legend class="fieldset-legend">Inputs</legend>
+
+			<ItemSelect bind:items={inputs} />
+		</fieldset>
+
+		<fieldset class="fieldset bg-base-100 border-base-300 rounded-box border p-4">
+			<legend class="fieldset-legend">Outputs</legend>
+
+			<ItemSelect bind:items={outputs} />
+		</fieldset>
+
+		<div class="flex flex-row-reverse gap-2 pt-2">
+			<button onclick={calculate} class="btn btn-primary btn-soft">Calculate</button>
+		</div>
+	</div>
+</div>
--- src/lib/components/profile/ProfileLayout.svelte
+++ src/lib/components/profile/ProfileLayout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import FactoryDetails from '@/lib/components/profile/FactoryDetails.svelte';
 	import OverviewWindow from '@/lib/components/profile/OverviewWindow.svelte';
 	import ProfileStage from '@/lib/components/profile/ProfileStage.svelte';
 </script>
+
+<FactoryDetails />
 
 <OverviewWindow />
 
