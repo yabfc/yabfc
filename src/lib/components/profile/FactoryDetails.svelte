21da4785038b7d9d6ac40169ee5d68bc34bbcbd6
tametsi <93092155+tametsi@users.noreply.github.com>
2026-02-09 09:08:28 +0100

    feat(ui): add factory creationmanagement UI skeleton (#44)
    * feat(ui): add item select component
    
    * feat(ui): add factory details skeleton
    
    * feat(ui): add electricity stat skeleton

---
+++ src/lib/components/profile/FactoryDetails.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import ItemSelect from '@/lib/components/shared/ItemSelect.svelte';
+	import Item from '@/lib/models/item';
+	import { ZapIcon } from '@lucide/svelte';
+
+	let inputs = $state<{ item: Item; amount: number }[]>([]),
+		outputs = $state<{ item: Item; amount: number }[]>([]);
+
+	function calculate() {
+		//
+	}
+</script>
+
+<div
+	class="fixed top-24 bottom-4 left-4 z-10 h-fit max-h-[calc(100%-var(--spacing)*28)] w-80 overflow-auto"
+>
+	<div class="rounded-box bg-base-200 p-2">
+		<h2 class="text-xl font-bold">This Factory</h2>
+
+		<fieldset class="fieldset bg-base-100 border-base-300 rounded-box min-w-0 border p-4">
+			<legend class="fieldset-legend">Inputs</legend>
+
+			<ItemSelect bind:items={inputs} />
+		</fieldset>
+
+		<fieldset class="fieldset bg-base-100 border-base-300 rounded-box border p-4">
+			<legend class="fieldset-legend">Outputs</legend>
+
+			<ItemSelect bind:items={outputs} />
+		</fieldset>
+
+		<ul class="py-2">
+			<li class="px-2">
+				<ZapIcon size="18" class="text-base-content/50 inline" />
+				<span class="sr-only">Electricity</span>
+				<b class="font-bold">13</b>
+				<span class="text-base-content/80">MW</span>
+			</li>
+		</ul>
+
+		<div class="flex flex-row-reverse gap-2 pt-2">
+			<button onclick={calculate} class="btn btn-primary btn-soft">Calculate</button>
+		</div>
+	</div>
+</div>
