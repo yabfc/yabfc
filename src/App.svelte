f535b8f9cab77e60742ecb23285daf4402d1b705
tametsi <93092155+tametsi@users.noreply.github.com>
2026-02-10 11:37:22 +0100

    feat(ui): add alert system (#50)
    - add alert system
    - add creation of alert if factory calculation fails

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Navbar from '@/lib/components/Navbar.svelte';
+	import Alerts from '@/lib/components/shared/Alerts.svelte';
 	import ThemeHandler from '@/lib/components/ThemeHandler.svelte';
 	import Router from '@/lib/router/Router.svelte';
 	import routes from '@/routes';
@@ -9,6 +10,8 @@
 	<Navbar />
 
 	<Router {routes} />
+
+	<Alerts />
 
 	<ThemeHandler />
 </main>
