715a3c618d1d0452d94cf443e4fd830bd5da4ca9
tametsi <93092155+tametsi@users.noreply.github.com>
2026-03-30 17:45:34 +0200

    feat: add empty factory route

---
+++ src/pages/Factory.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import active from '@/stores/active.svelte';
+	import profiles from '@/stores/profiles.svelte';
+	import { CableCarIcon } from '@lucide/svelte';
+
+	let { pathParams }: { pathParams: string[] } = $props();
+
+	$effect(() => {
+		// update active profile
+		active.profile = $profiles.find(x => x.id === pathParams[0]);
+	});
+</script>
+
+{#if active.profile}
+	<!--  -->
+{:else}
+	<div class="flex min-h-screen flex-col items-center justify-center gap-4 p-8 pt-28">
+		<div>
+			<CableCarIcon size="150" class="opacity-30" />
+		</div>
+
+		<h2 class="text-base-content/70 text-4xl font-bold">Profile not found.</h2>
+	</div>
+{/if}
--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,4 @@
+import Factory from '@/pages/Factory.svelte';
 import Index from '@/pages/Index.svelte';
 import Profile from '@/pages/Profile.svelte';
 import Settings from '@/pages/Settings.svelte';
@@ -15,5 +16,9 @@ export default [
 	{
 		route: '/p/([a-zA-Z0-9_-]+)',
 		entry: Profile,
+	},
+	{
+		route: '/f/([a-zA-Z0-9_-]+)',
+		entry: Factory,
 	},
 ] satisfies { route: string; entry: Component<{ pathParams: string[] }> | Component }[];
