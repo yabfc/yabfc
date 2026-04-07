a202ef5b3fe907ad17424e790a89418d84f17946
melo-afk <git@melo.run>
2026-04-07 21:52:22 +0200

    feat: use baseCraftingSpeed of machines in edge calculation

--- src/lib/factory/factory.ts
+++ src/lib/factory/factory.ts
@@ -128,8 +128,23 @@ export function calculateRecipeNodeModifier(
 
 	const runs = profile.settings.defaultDuration / recipe.duration;
 
-	const outputModifier = node.machines.reduce((sum, m) => sum + m.speed * m.efficiency, 0);
-	const inputModifier = node.machines.reduce((sum, m) => sum + m.speed, 0);
+	const outputModifier = node.machines.reduce(
+		(sum, m) =>
+			sum +
+			(profile.getMachineById(m.machineId)?.getBaseCraftingSpeed(profile.machineEffects) ??
+				1) *
+				m.speed *
+				m.efficiency,
+		0,
+	);
+	const inputModifier = node.machines.reduce(
+		(sum, m) =>
+			sum +
+			(profile.getMachineById(m.machineId)?.getBaseCraftingSpeed(profile.machineEffects) ??
+				1) *
+				m.speed,
+		0,
+	);
 
 	modifier.output = outputModifier * runs;
 	modifier.input = inputModifier * runs;
