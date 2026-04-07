208794eae017701fcf06c75fe09aa5535e466190
melo-afk <git@melo.run>
2026-04-07 22:42:42 +0200

    feat: readd item item picker

--- src/components/factory/FactoryPlanner.svelte
+++ src/components/factory/FactoryPlanner.svelte
@@ -2,21 +2,35 @@
 	import { calculateEdges, getRecipeChain, getResourceInputs } from '@/lib/factory/factory';
 	import active from '@/stores/active.svelte';
 	import factory from '@/stores/factory.svelte';
+	import ItemSelect from '@/components/shared/ItemSelect.svelte';
+	import { ZapIcon } from '@lucide/svelte';
+	import type Item from '@/lib/models/item';
+
+	let inputs = $state<{ item: Item; amount: number }[]>([]),
+		outputs = $state<{ item: Item; amount: number }[]>([]);
+
+	function clearFactory() {
+		factory.edges = [];
+		factory.inputs = {};
+		factory.outputs = {};
+		factory.recipeNodes = {};
+	}
 
 	async function calculate() {
 		if (!active.profile) return;
 
-		const out = [
-			{ id: 'nuclear-fuel-rod', amount: 10 },
-			{ id: 'iron-rod', amount: 10 },
-			{ id: 'advanced-circuit', amount: 10 },
-		];
+		if (outputs.length === 0) {
+			clearFactory();
+			return;
+		}
 
-		factory.outputs = Object.fromEntries(out.map(item => [item.id, item]));
+		factory.outputs = Object.fromEntries(
+			outputs.map(io => [io.item.id, { id: io.item.id, amount: io.amount }]),
+		);
 
 		const recipeChain = getRecipeChain(
 			active.profile,
-			out.map(x => x.id),
+			outputs.map(io => io.item.id),
 		);
 
 		factory.recipeNodes = Object.fromEntries(recipeChain.map(x => [x.id, x]));
@@ -35,7 +49,28 @@
 	class="fixed top-24 bottom-4 left-4 z-10 h-fit max-h-[calc(100%-var(--spacing)*28)] w-80 overflow-auto"
 >
 	<div class="rounded-box bg-base-200 p-2">
-		<h2 class="text-xl font-bold">Calculate Factory</h2>
+		<h2 class="text-xl font-bold">This Factory</h2>
+
+		<fieldset class="fieldset bg-base-100 border-base-300 rounded-box min-w-0 border p-4">
+			<legend class="fieldset-legend">Inputs</legend>
+
+			<ItemSelect bind:items={inputs} />
+		</fieldset>
+
+		<fieldset class="fieldset bg-base-100 border-base-300 rounded-box border p-4">
+			<legend class="fieldset-legend">Outputs</legend>
+
+			<ItemSelect bind:items={outputs} />
+		</fieldset>
+
+		<ul class="py-2">
+			<li class="px-2">
+				<ZapIcon size="18" class="text-base-content/50 inline" />
+				<span class="sr-only">Electricity</span>
+				<b class="font-bold">13</b>
+				<span class="text-base-content/80">MW</span>
+			</li>
+		</ul>
 
 		<div class="flex flex-row-reverse gap-2 pt-2">
 			<button onclick={calculate} class="btn btn-primary btn-soft">Calculate</button>
