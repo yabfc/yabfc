6cd8d5da586fd3d070c5fa21230e6e698c7fed73
tametsi <93092155+tametsi@users.noreply.github.com>
2026-04-16 22:22:05 +0200

    refactor: replace stage controls with custom controls

---
+++ src/components/factory/FactoryStageControls.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { FullscreenIcon, MinusIcon, PlusIcon } from '@lucide/svelte';
+	import { useStore } from '@xyflow/svelte';
+
+	let store = $derived(useStore());
+
+	const zoomIn = () => store.zoomIn(),
+		zoomOut = () => store.zoomOut(),
+		fitView = () => store.fitView();
+</script>
+
+<div class="bg-base-200 fixed bottom-4 left-1/2 z-10 flex -translate-x-1/2">
+	<div class="join">
+		<div class="tooltip" data-tip="Zoom in">
+			<button onclick={zoomIn} class="join-item btn btn-ghost btn-sm btn-square">
+				<PlusIcon size="18" />
+				<span class="sr-only">Zoom in</span>
+			</button>
+		</div>
+
+		<div class="tooltip" data-tip="Zoom out">
+			<button onclick={zoomOut} class="join-item btn btn-ghost btn-sm btn-square">
+				<MinusIcon size="18" />
+				<span class="sr-only">Zoom out</span>
+			</button>
+		</div>
+
+		<div class="tooltip" data-tip="Fit into view">
+			<button onclick={fitView} class="join-item btn btn-ghost btn-sm btn-square">
+				<FullscreenIcon size="18" />
+				<span class="sr-only">Fit into view</span>
+			</button>
+		</div>
+	</div>
+</div>
