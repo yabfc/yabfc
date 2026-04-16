bf56ca92754bc6983a06132a988aa0ded25c2318
tametsi <93092155+tametsi@users.noreply.github.com>
2026-04-16 22:27:13 +0200

    feat: add import/export button mockup

--- src/components/factory/FactoryStageControls.svelte
+++ src/components/factory/FactoryStageControls.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { FullscreenIcon, MinusIcon, PlusIcon } from '@lucide/svelte';
+	import { DownloadIcon, FullscreenIcon, ImportIcon, MinusIcon, PlusIcon } from '@lucide/svelte';
 	import { useStore } from '@xyflow/svelte';
 
 	let store = $derived(useStore());
@@ -7,6 +7,14 @@
 	const zoomIn = () => store.zoomIn(),
 		zoomOut = () => store.zoomOut(),
 		fitView = () => store.fitView();
+
+	const importFactory = () => {
+		//
+	};
+
+	const exportFactory = () => {
+		//
+	};
 </script>
 
 <div class="bg-base-200 fixed bottom-4 left-1/2 z-10 flex -translate-x-1/2">
@@ -22,6 +30,20 @@
 			<button onclick={zoomOut} class="join-item btn btn-ghost btn-sm btn-square">
 				<MinusIcon size="18" />
 				<span class="sr-only">Zoom out</span>
+			</button>
+		</div>
+
+		<div class="tooltip" data-tip="Import">
+			<button onclick={importFactory} class="join-item btn btn-ghost btn-sm btn-square">
+				<ImportIcon size="18" />
+				<span class="sr-only">Import</span>
+			</button>
+		</div>
+
+		<div class="tooltip" data-tip="Export">
+			<button onclick={exportFactory} class="join-item btn btn-ghost btn-sm btn-square">
+				<DownloadIcon size="18" />
+				<span class="sr-only">Export</span>
 			</button>
 		</div>
 
